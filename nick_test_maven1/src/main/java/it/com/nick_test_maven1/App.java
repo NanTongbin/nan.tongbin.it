8f648e46d916ac8b451f34a6c386017ab0f6277c
NanTongbin <33367330+NanTongbin@users.noreply.github.com>
2022-10-27 21:57:37 +0800

    Add files via upload

---
+++ nick_test_maven1/src/main/java/it/com/nick_test_maven1/App.java
@@ -0,0 +1,16 @@
+package it.com.nick_test_maven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Cat cat  =new Cat();
+        System.out.println(cat);
+    }
+}
---
+++ nick_test_maven1/src/main/java/it/com/nick_test_maven1/Cat.java
@@ -0,0 +1,15 @@
+package it.com.nick_test_maven1;
+
+public class Cat {
+
+	int age=8;
+	String name="tiantian";
+	
+	public int getAge() {
+		return this.age;
+	}
+	public String getName() {
+		return name;
+	}
+	
+}
